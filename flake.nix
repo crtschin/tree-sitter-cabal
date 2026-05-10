4df461787f87268aab4138070206cd09345aa19a
Curtis Chin Jen Sem <csochinjensem@gmail.com>
2026-05-10 16:50:12 +0200

    Simplify nix setup

--- flake.nix
+++ flake.nix
@@ -18,89 +18,41 @@
           overlays = [ ];
         };
 
-        pkgDirs = [
-          "tree-sitter-cabal"
-          "tree-sitter-cabal-project"
-        ];
-        pkgDirsStr = builtins.concatStringsSep " " pkgDirs;
-
-        nodeModules = pkgs.stdenv.mkDerivation (finalAttrs: {
-          pname = "tree-sitter-cabal-workspace-node-modules";
-          version = "0.0.0";
-          src = ./.;
-
-          pnpmDeps = pkgs.fetchPnpmDeps {
-            inherit (finalAttrs) pname version src;
-            fetcherVersion = 3;
-            hash = "sha256-FK8Q2T6l2r5l1e49gHKfpx1JugHSsNRG9vEBgNMAmbI=";
+        buildTreeSitterPkg =
+          { pname, language }:
+          pkgs.tree-sitter.buildGrammar {
+            inherit language;
+            version = "0.1.0";
+            src = ./${pname};
+            generate = true;
           };
 
-          nativeBuildInputs = with pkgs; [
-            (node-gyp.override { nodejs = nodejs_22; })
-            nodejs_22
-            pnpm
-            pnpmConfigHook
-            python3
-          ];
+        treeSitterCabal = buildTreeSitterPkg {
+          pname = "tree-sitter-cabal";
+          language = "cabal";
+        };
 
-          env.npm_config_nodedir = pkgs.nodejs_22;
-
-          buildPhase = ''
-            runHook preBuild
-            for treeSitterDir in node_modules/.pnpm/tree-sitter@*/node_modules/tree-sitter; do
-              if [ -d "$treeSitterDir" ]; then
-                pushd "$treeSitterDir"
-                node-gyp rebuild
-                popd
-              fi
-            done
-            for pkgDir in ${pkgDirsStr}; do
-              pushd "$pkgDir"
-              node-gyp rebuild
-              popd
-            done
-            runHook postBuild
-          '';
-
-          installPhase = ''
-            runHook preInstall
-            mkdir -p $out
-            cp -a node_modules $out/node_modules
-            for pkgDir in ${pkgDirsStr}; do
-              mkdir -p $out/$pkgDir
-              cp -a $pkgDir/node_modules $out/$pkgDir/node_modules
-              cp -a $pkgDir/build        $out/$pkgDir/build
-            done
-            runHook postInstall
-          '';
-        });
+        treeSitterCabalProject = buildTreeSitterPkg {
+          pname = "tree-sitter-cabal-project";
+          language = "cabal_project";
+        };
       in
       {
+        packages = {
+          tree-sitter-cabal = treeSitterCabal;
+          tree-sitter-cabal-project = treeSitterCabalProject;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (node-gyp.override { nodejs = nodejs_22; })
             haskellPackages.cabal-fmt
             just
             nixfmt
             nodejs_22
-            pnpm
             prettier
-            python3
             tree-sitter
             typescript-language-server
           ];
-
-          env.npm_config_nodedir = pkgs.nodejs_22;
-
-          shellHook = ''
-            ln -snf ${nodeModules}/node_modules ./node_modules
-            for pkgDir in ${pkgDirsStr}; do
-              ln -snf ${nodeModules}/$pkgDir/node_modules ./$pkgDir/node_modules
-              mkdir -p ./$pkgDir/build/Release
-              binding="tree_sitter_$(echo "$pkgDir" | tr - _)_binding.node"
-              ln -snf ${nodeModules}/$pkgDir/build/Release/$binding ./$pkgDir/build/Release/$binding
-            done
-          '';
         };
       }
     );
